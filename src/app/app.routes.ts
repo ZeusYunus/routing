ee5af2082e4f8286df383f83705820cf7589fa4e
Nizaam Yunus <nizaamcoleyunus18@gmail.com>
2025-08-17 15:11:10 +0200

    Rendering routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Route } from "@angular/router";
-import { TaskComponent } from "./tasks/task/task.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 export const routes: Route[] = [
     {
         path: 'tasks', //<your-domain>/tasks
-        component: TaskComponent,
+        component: TasksComponent,
     },
 ];
